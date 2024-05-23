bcb6c2bf7ca9fd5bf28155c23130c8859b487fc1
Tuyen Dang <tuyendang878@gmail.com>
2024-05-23 15:00:02 +0700

    Designing Signup Component-I Part-5

--- client/src/Components/Signup/Signup.tsx
+++ client/src/Components/Signup/Signup.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+import styles from "./Signup.module.css";
+import { Link } from "react-router-dom";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    // full page
+    <div className={styles.container}>
+      {/* container - Form */}
+      <div className={styles.formContainer}>
+        <h2>Sign Up ...</h2>
+        {/* control input */}
+        <div className={styles.inputContainer}>
+          <input placeholder="Enter your username ..." type="text" />
+          <input placeholder="Enter your email ..." type="text" />
+          <input placeholder="Enter your password ..." type="text" />
+          <button>Sign Up</button>
+        </div>
+        <Link to="/">Already have an account ? Loin</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
