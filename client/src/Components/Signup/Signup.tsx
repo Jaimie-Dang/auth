b442d289ea3f9e7e4df45a630a850833e9cb622f
Tuyen Dang <tuyendang878@gmail.com>
2024-05-23 15:49:44 +0700

    Designing Signup Component - II Part-6

--- client/src/Components/Signup/Signup.tsx
+++ client/src/Components/Signup/Signup.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 
 import styles from "./Signup.module.css";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [userDetails, setuserDetails] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>): void {
+    const { name, value } = event.target;
+    setuserDetails((prev) => ({ ...prev, [name]: value }));
+    console.log(userDetails);
+  }
+
   return (
     // full page
     <div className={styles.container}>
@@ -12,9 +24,27 @@ const Signup = () => {
         <h2>Sign Up ...</h2>
         {/* control input */}
         <div className={styles.inputContainer}>
-          <input placeholder="Enter your username ..." type="text" />
-          <input placeholder="Enter your email ..." type="text" />
-          <input placeholder="Enter your password ..." type="text" />
+          <input
+            value={userDetails.username}
+            placeholder="Enter your username ..."
+            type="text"
+            name="username"
+            onChange={handleInputChange}
+          />
+          <input
+            value={userDetails.email}
+            placeholder="Enter your email ..."
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+          />
+          <input
+            value={userDetails.password}
+            placeholder="Enter your password ..."
+            type="password"
+            name="password"
+            onChange={handleInputChange}
+          />
           <button>Sign Up</button>
         </div>
         <Link to="/">Already have an account ? Loin</Link>
