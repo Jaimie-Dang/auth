8f9bf5f11d2617c34760b7c21b2ce5490d1e214b
Tuyen Dang <tuyendang878@gmail.com>
2024-05-23 13:17:59 +0700

    Setting Up Routes Part-3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,18 @@ import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// nav bar
 const App = () => {
   return (
-    <div>
-      <Login />
-      <Signup />
-      <ForgorPassword />
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/forget-password" element={<ForgorPassword />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 };
 
