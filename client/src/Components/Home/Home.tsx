e4353a765a056269d7fb2666288f95148dcd17b3
Tuyen Dang <tuyendang878@gmail.com>
2024-05-27 19:38:51 +0700

    Home Component UI - I Part-33

--- client/src/Components/Home/Home.tsx
+++ client/src/Components/Home/Home.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import styles from "./Home.module.css";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={styles.container}>
+      <nav>
+        <span>Hello user</span>
+        <button
+          onClick={() => {
+            localStorage.clear();
+            navigate("/");
+            toast.success("User logged out");
+          }}
+          className={styles.logoutBtn}
+        >
+          LOGOUT
+        </button>
+      </nav>
+      <div className={styles.mainContainer}>
+        <div>
+          User Details
+          <p>Username: name</p>
+          <p>Email: email</p>
+        </div>
+        <div className={styles.editContainer}>
+          Update User
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
